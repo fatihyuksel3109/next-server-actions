23a40442547fc5d760b5010e749a2bb99e093222
Fatih Yüksel <fatihyuksel3109@gmail.com>
2023-11-29 17:32:15 +0300

    layout, home, types, data composed

---
+++ types/type.ts
@@ -0,0 +1,16 @@
+export interface AnimeProp {
+    id: string;
+    name: string;
+    image: {
+      original: string;
+    };
+    kind: string;
+    episodes: number;
+    episodes_aired: number;
+    score: string;
+  }
+  
+  interface Prop {
+    anime: AnimeProp;
+    index: number;
+  }
